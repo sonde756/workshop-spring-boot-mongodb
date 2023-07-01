fa0cc2eedd6a3b495310badab42b3b903b334ebf
sonde756 <edsonrafael756@gmail.com>
2023-07-01 12:05:27 -0300

    first commit

---
+++ src/test/java/com/sonde/workshopmongodb/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonde.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
